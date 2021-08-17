4dcd018fad4c71712ae7e09fe96c7e10d0ab370e
am-c-whjr <84730467+am-c-whjr@users.noreply.github.com>
2021-08-17 12:34:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,50 @@
-var speechRecognition = window.webkitSpeechRecognition;
-var recognition = new speechRecognition;
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var Textbox = document.getElementById("textbox");
 
 function start() {
-    document.getElementById("textbox").innerHTML = "";
+    Textbox.innerHTML = "";
     recognition.start();
 }
-
-recognition.onresult = function run(event) {
+recognition.onresult = function (event) {
     console.log(event);
     var Content = event.results[0][0].transcript;
-    document.getElementById("textbox").innerHTML = Content;
-    console.log(content);
-    speak();
+    Textbox.innerHTML = Content;
+    console.log(Content);
+    if (Content == "take my selfie") {
+        console.log("taking selfie --- ");
+        speak();
+    }
 }
 
 function speak() {
-    var synth = window.speachSynthesis;
-    speak_data = "taking your selfie in 5 seconds";
-    var UtterThis = new speechSynthesisUtterance(speak_data);
-    synth.speak(UtterThis);
-    Webcam.attach(camera);
+    var Synth = window.SpeechSynthesis;
+    speak_data = "Taking your selfie in 5 seconds";
+    var UtterThis = new SpeechSynthesisUtterance(speak_data);
+    Synth.speak(utterThis);
+    webcam.attach(camera);
+    setTimeout(function () {
+        take_selfie();
+        save();
+    }, 5000);
 }
+camera = document.getElementById("camera");
 Webcam.set({
     width: 360,
     height: 250,
     image_format: 'jpeg',
-    jpeg_quality: 90,
+    jpeg_quality: 90
 });
-camera = document.getElementById("camera");
+
+function take_selfie() {
+    Webcam.snap(function (data_url) {
+        document.getElementById("result").innerHTML = "<img id='selfie_image' src='" + data_url + "'>";
+    });
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
